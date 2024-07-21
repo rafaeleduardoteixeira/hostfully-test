c5490693516cab0cb3aa815e302e40d7a89dd1c7
Rafael Teixeira <rafaeleduardoteixeira@gmail.com>
2024-07-21 19:47:47 -0300

    chore: create and run initial project using React

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders React app start", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/React app start/i);
   expect(linkElement).toBeInTheDocument();
 });
