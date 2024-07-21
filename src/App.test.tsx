3bb979a50da8eab53ab8836a0f0d99a4f5e4d0e9
Rafael Teixeira <rafaeleduardoteixeira@gmail.com>
2024-07-21 20:32:16 -0300

    chore: add prettier and configure it

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders React app start", () => {
+test('renders React app start', () => {
   render(<App />);
   const linkElement = screen.getByText(/React app start/i);
   expect(linkElement).toBeInTheDocument();
