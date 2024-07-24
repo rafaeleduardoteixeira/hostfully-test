9bbea1bbbc75bb5105035655776667af9905a1f0
Rafael Teixeira <rafaeleduardoteixeira@gmail.com>
2024-07-23 21:11:32 -0300

    reafactor: move the menu inside the react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import createdRoutes from './constants/router';
-import { Header } from './components/Header/Header';
 import { AppContainer } from './App.styles';
 import AppContextProvider from './Context/App.context';
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <AppContainer>
       <AppContextProvider>
-        <Header />
         <RouterProvider router={createdRoutes} />
       </AppContextProvider>
     </AppContainer>
