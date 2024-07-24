a57d4a2634882a0572ffafcd10186a10ca9a149a
Rafael Teixeira <rafaeleduardoteixeira@gmail.com>
2024-07-23 21:14:07 -0300

    refactor: add navigation on menu

--- src/components/DropDownMenu/DropDownMenu.tsx
+++ src/components/DropDownMenu/DropDownMenu.tsx
@@ -1,10 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import { Container, Item } from './DropDownMenu.styles';
 
 export const DropDownMenu = (): JSX.Element => {
+  const navigate = useNavigate();
+
+  const handleTrips = () => {
+    navigate('/my-trips');
+  };
+
   return (
     <Container>
-      <Item>Profile</Item>
-      <Item>Trips</Item>
+      <Item onClick={handleTrips}>Trips</Item>
     </Container>
   );
 };
