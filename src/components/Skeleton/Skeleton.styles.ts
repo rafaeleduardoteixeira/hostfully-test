053675768be173a623c858b059ddb0f6c022a66a
Rafael Teixeira <rafaeleduardoteixeira@gmail.com>
2024-07-23 21:15:31 -0300

    refactor: add new props on skeleton

--- src/components/Skeleton/Skeleton.styles.ts
+++ src/components/Skeleton/Skeleton.styles.ts
@@ -2,13 +2,16 @@ import styled from 'styled-components';
 
 interface SkeletonProps {
   width?: string;
+  height?: string;
+  padding?: string;
 }
 
 export const Container = styled.div<SkeletonProps>`
   width: ${(props) => props.width || '100%'};
-  height: 100%;
+  height: ${(props) => props.height || '100%'};
   border-radius: 10px;
   animation: skeleton-loading 1s linear infinite alternate;
+  padding: ${(props) => props.padding || '0'};
   @keyframes skeleton-loading {
     0% {
       background-color: hsl(200, 10%, 90%);
--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -3,6 +3,8 @@ import { Container } from './Skeleton.styles';
 
 interface SkeletonProps {
   width?: string;
+  height?: string;
+  padding?: string;
 }
 
 export const Skeleton = (props: SkeletonProps) => {
